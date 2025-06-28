c9c7500da0046f558576d8d71f1d46456f4fd042
akmet <10135260+akmet@users.noreply.github.com>
2025-06-28 18:53:28 +0200

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,41 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   base: "./",
   plugins: [vue()],
   // prevent vite from obscuring rust errors
   clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
   server: {
+    // make sure this port matches the devUrl port in tauri.conf.json file
+    port: 5173,
+    // Tauri expects a fixed port, fail if that port is not available
     strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: 'ws',
+        host,
+        port: 1421,
+      }
+      : undefined,
+
+    watch: {
+      // tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
   },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: ['VITE_', 'TAURI_ENV_PLATFORM', 'TAURI_ENV_ARCH', 'TAURI_ENV_FAMILY', 'TAURI_ENV_PLATFORM_VERSION', 'TAURI_ENV_PLATFORM_TYPE', 'TAURI_ENV_DEBUG'],
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
     // don't minify for debug builds
     minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
