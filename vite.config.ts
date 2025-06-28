e18ce6ef65528eb6db10d6cf2dd74a8a954ce9e0
akmet <10135260+akmet@users.noreply.github.com>
2025-06-28 21:54:25 +0200

    based on clean repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-  base: "./",
   plugins: [vue()],
   // prevent vite from obscuring rust errors
   clearScreen: false,
